6d50978b89cf7bcf3fb180ec90fd1631ceafda9b
Murtuza Saifee <msaifee@firstorion.com>
2025-06-19 18:43:42 +0400

    docker executor

---
+++ src/dsa_agent/utils/docker_utils.py
@@ -0,0 +1,19 @@
+from autogen_ext.code_executors.docker import DockerCommandLineCodeExecutor
+from configs.constants import DOCKER_WORK_DIR, DOCKER_TIMEOUT
+
+def get_docker_executor():
+    docker=DockerCommandLineCodeExecutor(
+        work_dir=DOCKER_WORK_DIR,
+        timeout=DOCKER_TIMEOUT
+    )
+    return docker
+ 
+ 
+async def start_docker_executor(docker):
+      await docker.start()
+      print("Docker executor started.")
+      
+      
+async def stop_docker_executor(docker):
+      await docker.stop()
+      print("Docker executor stopped.")
