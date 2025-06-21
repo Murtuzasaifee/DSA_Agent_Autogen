b8215447851bd72cf02524b0b6938a48324369a1
Murtuza Saifee <msaifee@firstorion.com>
2025-06-21 15:11:36 +0400

    updated with constants

--- src/dsa_agent/teams/dsa_solver_team.py
+++ src/dsa_agent/teams/dsa_solver_team.py
@@ -1,17 +1,18 @@
-from autogen_agentchat.messages import TextMessage
 from autogen_agentchat.teams import RoundRobinGroupChat
 from autogen_agentchat.conditions import TextMentionTermination
-from autogen_agentchat.base import TaskResult
+from src.dsa_agent.configs.constants import TEXT_MENTION_TERMINATION, MAX_TURNS  
+
 
 
 
 def get_dsa_solver_team(problem_solver_expert, code_executor_agent):
     
-    termination_condition = TextMentionTermination('STOP')
+    termination_condition = TextMentionTermination(TEXT_MENTION_TERMINATION)
     
     team = RoundRobinGroupChat(
-    participants=[problem_solver_expert, code_executor_agent],
-    termination_condition=termination_condition,
-    max_turns=15)
+        participants=[problem_solver_expert, code_executor_agent],
+        termination_condition=termination_condition,
+        max_turns=MAX_TURNS,
+    )
     
     return team
